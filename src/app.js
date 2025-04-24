27d87ba8771c4511453a2ae1d4b950ffea4d44c1
Gourav <gourav@gmail.com>
2025-04-24 06:48:18 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
-
 const app = express();
 
-app.use("/test",(req, res) => {
-    res.send("Namaste form the server!");
+// app.get("/user/:userID", (req, res) => {
+//     console.log(req.params);
+//     res.send("ID is " + req.params.userID);
+//     // res.send("Its working fine");
+// });
+app.use(
+    "/user",
+    (req, res , next) => {
+    console.log("Sending Request 1!!!");
+    next();
+    // res.send("Its working fine");
+        
+
+},
+    (req, res) => {
+     console.log("Sending Request 2!!!");
+     res.send("Its working fine 2");
 })
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
