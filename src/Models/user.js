6874b39c5a4971f58e7dd0dcdbfa6cc3479a6e26
Gourav <gourav@gmail.com>
2025-04-29 14:34:06 +0530

    Schema Validation

--- src/Models/user.js
+++ src/Models/user.js
@@ -1,27 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose"); // Importing mongoose to interact with MongoDB
 
 const userSchema = new mongoose.Schema(
     {
         firstName: {
             type: String,
+            required: true,
         },
         lastName: {
             type: String,
+            
+
         },
         email: {
             type: String,
+            required: true,
+            unique: true, // Ensures that the email is unique in the database
+            lowercase: true, // Converts the email to lowercase before saving
+            trim: true, // Removes any leading or trailing whitespace
+
         },
         password: {
             type: String,
+            required: true,
+
         },
         age: {
             type: Number,
+            min: 18, // Minimum age is 18
+
         },
         gender: {
             type: String,
+            validate(value) {
+                if (!["male", "female", "other"].includes(value)) {
+                    throw new Error("Invalid gender value");
+                }
+            }
+        },
+        photoUrl: {
+            type: String,
+            default: "https://www.w3schools.com/howto/img_avatar.png", // Default photo URL
+        },
+        about: {
+            type: String,
+            default: "Hey there! This is the default about.",
+        },
+        skills: {
+            type: [String], // Array of strings
         }
-    }
-    
+    },
+        {
+            timestamps:true
+        }
+   
 );
 
 const userModel = mongoose.model("User" , userSchema);
--- src/app.js
+++ src/app.js
@@ -6,12 +6,11 @@ const User = require("./Models/user");
 
 app.use(express.json()); 
 
+//creating a new user
 app.post("/signup", async (req, res) => {
 
      //Creating instance of User Model
     const user = new User(req.body);
-    //Creating a new instance of User Model
-    
     try {
         await user.save(); // Saving the user to the database
         res.send("User Created Successfully!!!");
@@ -21,6 +20,65 @@ app.post("/signup", async (req, res) => {
     }
 
 
+});
+
+//Get one user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.email;
+
+    try {
+        const user = await User.find({ email: userEmail });
+        if (user.length === 0) {
+            res.status(404).send("User not found");
+        }
+        res.send(user);
+    }
+    catch (err){
+        res.status(400).send("Something went wrong");
+    }
+
+});
+
+//Feed API - get all the users from the database.
+app.get("/feed", async (req, res) => {
+    // const userEmail = req.body.email;
+    // const oneUser = await User.findOne({ email: userEmail });
+    // res.send(oneUser);
+
+    const users = await User.find({});
+    if (!users) {
+            res.status(404).send("User not found");
+        }
+    res.send(users);
+})
+
+//Delete a user by ID
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userID;
+    try {
+        const dltUser = await User.findByIdAndDelete(userId);
+        res.send("Deleted");
+    }
+    catch (err) {
+        res.status(404).send("Dikkat ho gayi hai kuch");
+    }
+
+})
+
+//Update a user data
+app.patch("/user", async (req, res) => {
+    const userID = req.body.userID;
+    const data = req.body;
+    try {
+        await User.findByIdAndUpdate({ _id: userID }, data, {
+            returnDocument: "after",
+            runValidators: true,
+        });
+        res.send("Updated Successfully!!!");
+    }
+    catch (err) {
+        res.status(404).send("Dikkat ho gayi hai kuch" , err.message);
+    }
 });
 
 connectDB()
